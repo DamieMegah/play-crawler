245396c767f20e7599a35995250d57b033dc9cde
Damiemegah <dareyjustice@gmail.com>
2026-07-03 12:48:47 +0100

    change logo icon to fontawesome icon

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,11 +1,17 @@
 import { Link } from "react-router-dom";
 import "../css/Logo.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay } from "@fortawesome/free-solid-svg-icons";
 function Logo() {
   return (
     <div>
       <Link to="/" className="nav-brand">
         PL
-        <span className="play">▶</span>Y<span>crawler</span>
+        <span className="play-box">
+          {" "}
+          <FontAwesomeIcon icon={faPlay} className="play" />
+        </span>
+        Y<span>crawler</span>
       </Link>
     </div>
   );
